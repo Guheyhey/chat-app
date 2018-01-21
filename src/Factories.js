f7fd403105bbf09ce64a096883de38e0179bb494
Yiqiao Liao <joeglancith@hotmail.com>
2018-01-20 20:18:37 -0500

    Finished Factories.js

--- src/Factories.js
+++ src/Factories.js
@@ -1,3 +1,5 @@
+const uuidv4 = require('uuid/v4');
+
 /**
  * createUSer
  * Creates a user
@@ -6,6 +8,12 @@
  * @param {object} 
  *   name {string}
  */
+const createUser = ({name} = {}) => {
+  return {
+    id: uuidv4(),
+    name: name
+  }
+}
 
 
 /**
@@ -19,6 +27,14 @@
  *	 message {string}
  *   sender {string}
  */
+const createMessage = ({message, sender} = {}) => {
+  return {
+    id: uuidv4(),
+    time: getTime(new Data(Date.now())),
+    message,
+    sender
+  }
+}
 
 
 /**
@@ -36,3 +52,49 @@
  *   name {string}
  *   users {Array.string}
  */
+const createChat = ({messages = [], name = "Community", users = []} = {}) => {
+  return {
+    id: uuidv4(),
+    name,
+    messages,
+    users,
+    typingUsers: []
+  }
+}
+
+// // ES5 version of above code
+// var createChat = function createChat() {
+//   var _ref3 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+//       _ref3$messages = _ref3.messages,
+//       messages = _ref3$messages === undefined ? [] : _ref3$messages,
+//       _ref3$name = _ref3.name,
+//       name = _ref3$name === undefined ? "Community" : _ref3$name,
+//       _ref3$users = _ref3.users,
+//       users = _ref3$users === undefined ? [] : _ref3$users;
+
+//   return {
+//     id: uuidv4(),
+//     name: name,
+//     messages: messages,
+//     users: users,
+//     typingUsers: []
+//   };
+// }
+
+
+
+/**
+ * getTime
+ * @param date {Date}
+ * @return a string represented in 24hr time i.e. '11:30:30'
+ */
+const getTime = (date) => {
+  return `${date.getHours()}:${("0" + date.getMinutes()).slice(-2)}:${("0" + date.getSeconds()).slice(-2)}`
+}
+
+
+module.exports = {
+	createChat,
+	createMessage,
+	createUser
+}
