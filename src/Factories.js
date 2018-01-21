8b08c3398d2d06cdebd7f873419566ac230d4ea5
Yiqiao Liao <joeglancith@hotmail.com>
2018-01-20 19:51:56 -0500

    Create instance factories in Factories.js

---
+++ src/Factories.js
@@ -0,0 +1,38 @@
+/**
+ * createUSer
+ * Creates a user
+ * @prop id {string}
+ * @prop name {string}
+ * @param {object} 
+ *   name {string}
+ */
+
+
+/**
+ * createMessage
+ * Creates a messages object.
+ * @prop id {string}
+ * @prop time {Date} the time in 24hr format i.e. 14:22
+ * @prop message {string} actual string message
+ * @prop sender {string} sender of the message
+ * @param {object} 
+ *	 message {string}
+ *   sender {string}
+ */
+
+
+/**
+ * createChat
+ * Creates a Chat object
+ * @prop id {string}
+ * @prop name {string}
+ * @prop messages {Array.Message}
+ * @prop users {Array.string}
+ * @prop addMessage {function} adds message to chat.
+ * @prop addTypingUser {function} adds a username to typing users of chat.
+ * @prop removeTypingUser {function} removes a username to typing users of chat.
+ * @param {object} 
+ *   messages {Array.Message}
+ *   name {string}
+ *   users {Array.string}
+ */
