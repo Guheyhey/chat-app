5fde7f9b40a4ef6f62d07e456f31840306554552
Yiqiao Liao <joeglancith@hotmail.com>
2018-01-20 19:25:48 -0500

    First Commit

---
+++ src/components/Layout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+
+const socketUrl = 'http://192.168.1.13:3231';
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      socket: null
+    };
+  }
+
+  componentWillMount() {
+    this.initSocket();
+  }
+
+  initSocket = () => {
+    const socket = io(socketUrl);
+    socket.on('connect', () => {
+      console.log("Connected!");
+    });
+    this.setState({socket: socket});
+  }
+
+  render() {
+    const { title } = this.props;
+    return (
+      <div className="container">
+        {title}
+      </div>
+    );
+  }
+}
+
+export default Layout;
